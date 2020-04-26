ac424e54c0605f69c636d98cadca54698f185dd6
Szabolcs Rák <rakszabi@gmail.com>
2020-04-26 18:34:02 +0200

    heroku test

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ db.on('error', (err) => {
     console.log(err);
 });
 
+app.use(function( req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // Body Parser Middleware
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
